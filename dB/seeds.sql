76444d81032bd2b4a0203c4aef7cae4b89a5bb1c
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-09-17 10:38:01 -0500

    Update seeds.sql
    updated seeds was wondering if its just an array not sure if they gave us vales or am I missing something

---
+++ dB/seeds.sql
@@ -0,0 +1,9 @@
+/* Must ADD the Values here I dont understand where they come from is it just an array*/
+INSERT INTO department(department_name)
+VALUES();
+
+INSERT INTO role( title, salary, department_id)
+VALUES();
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)    
+VALUES();
