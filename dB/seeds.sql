fac39db9a189082eb43333073fe301041d44a3a7
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-09-26 18:17:34 -0500

    Update seeds.sql
    Fixed a double ))

--- dB/seeds.sql
+++ dB/seeds.sql
@@ -1,4 +1,4 @@
-/* Must ADD the Values here I dont understand where they come from is it just an array?*/
+/* Must ADD the Values here I dont understand where they come from is it just an open array?*/
 /*Must include the values for the department, role, and employee tables.*/
 
 /* google SQL insert to figure out what to put in there*/
@@ -6,7 +6,7 @@
 INSERT INTO department(department_id, department_name, total_utilized_budget)  
 VALUES ("Department ID", "Department Name", "Total Utilized Budget");
 
-INSERT INTO role(role_id, title, salary, department_id))
+INSERT INTO role(role_id, title, salary, department_id)
 VALUES ("Role ID", "Title", "Salary", "Department ID");
 
 INSERT INTO employee(Employee_id,first_name, last_name, role_id, manager_id)    
